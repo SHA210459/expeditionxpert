2b4d5da87811160048be8987823aafca2cb173e0
Raiyan <raiyangame1@gmail.com>
2024-04-23 16:21:07 +0200

    Initial commit

---
+++ src/main/java/com/example/expeditionxpert/ExpeditionxpertApplication.java
@@ -0,0 +1,13 @@
+package com.example.expeditionxpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpeditionxpertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpeditionxpertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expeditionxpert/ExpeditionxpertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expeditionxpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpeditionxpertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
